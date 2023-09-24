96b3e5cae598da0b407873777e5592cff6255fe0
Raiden <86716536+Raiden1233@users.noreply.github.com>
2023-09-24 22:54:07 +0530

    Update reaction_role.py

--- reaction_role.py
+++ reaction_role.py
@@ -1,7 +1,13 @@
-# Note that you have to copy and paste all this code into yours or if you are more advance feel free to do whatever you want. Enjoy~!
+# Note that you have to copy and paste all this code into yours or if you are more advanced feel free to do whatever you want. Enjoy~!
 # This code is not specifically for 1 server your bot can execute this code on any server 
 
-@discord.ui.button(label= '✅', custom_id = "Announcement", style = discord.ButtonStyle.gray)
+class Roles(discord.ui.View):
+  def __init__(self, role_n):
+    self.role_n = role_n
+    super().__init__(timeout = None)
+  
+
+  @discord.ui.button(label= '✅', custom_id = "Announcement", style = discord.ButtonStyle.gray)
   async def button1(self, interaction, button):
     _role = discord.utils.get(interaction.guild.roles, name= self.role_n)
     role = _role.id #put your role id here
@@ -15,7 +21,7 @@
 
 
 
-@Raiden.tree.command(name='reaction_role', description='Provide the role name') # Kindly Replace  @Raiden with @bot or the name of your client
+@Raiden.tree.command(name='reaction_role', description='Provide the role name') # kindly replace @Raiden with your client name or leave it as @bot instead
 async def reaction_role(ctx: discord.Interaction, role_name: str, description_of_role: str):
   embed = discord.Embed(title = to_upper(role_name), description = description_of_role)
   r = discord.utils.get(ctx.guild.roles, name= role_name)
